728a1aea651274119fa60bf9693876a0b554e888
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-29 13:32:06 +0530

    ImageBackground in react native

--- basic/app/index.tsx
+++ basic/app/index.tsx
@@ -1,85 +1,35 @@
-import { useState } from "react";
-import {
-  Text,
-  View,
-  Button,
-  Switch,
-  ScrollView,
-  TextInput,
-  Pressable,
-  Image,
-} from "react-native";
+import React from 'react';
+import {ImageBackground, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
-function HomeScreen() {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const [text, setText] = useState("");
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
 
-  const pressMe = () => {
-    alert("Button Pressed");
-  };
-  const [timesPressed, setTimesPressed] = useState(0);
+const App = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container} edges={['left', 'right']}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Inside</Text>
+      </ImageBackground>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
 
-  let textLog = "";
-  if (timesPressed > 1) {
-    textLog = timesPressed + "x onPress";
-  } else if (timesPressed > 0) {
-    textLog = "onPress";
-  }
-  return (
-    <ScrollView>
-      {/* {
-        [...Array(30)].map((_,i)=>(
-          <Text key={i} style={{fontSize:24}}>
-            Hello From Home Screen {i+1}
-          </Text>
-        ))
-      } */}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+});
 
-      {/* <Text style={{ fontSize: 20, fontWeight: 'bold',marginBottom:20 }}>Home Screen</Text> */}
-
-      {/* <Button title='Click me' onPress={pressMe} /> */}
-
-      {/* <Switch
-          trackColor={{false: '#767577', true: '#81b0ff'}}
-          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-          ios_backgroundColor="#3e3e3e"
-          onValueChange={toggleSwitch}
-          value={isEnabled}
-        /> */}
-
-      {/* <TextInput
-        style={{ backgroundColor: "aqua", borderWidth: 2 }}
-        value={text}
-        onChangeText={setText}
-        keyboardType="default"
-        multiline
-        numberOfLines={4}
-        placeholder="Enter Your Text"
-      /> */}
-
-      {/* <Pressable
-        onPress={() => {
-          setTimesPressed((current) => current + 1);
-        }}
-       
-      >
-        {({ pressed }) => (
-          <Text >{pressed ? "Pressed!" : "Press Me"}</Text>
-        )}
-      </Pressable>
-      <View >
-        <Text testID="pressable_press_console">{textLog}</Text>
-      </View> */}
-
-      <Image
-        source={{
-          uri: "https://imgs.search.brave.com/ACedRZHztn-OEwyhM1B15tdkWFNDmr_vu6lbM9Pyr10/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4t/ZnJvbnQuZnJlZXBp/ay5jb20vaG9tZS9h/bm9uLXJ2bXAvY3Jl/YXRpdmUtc3VpdGUv/cGhvdG9ncmFwaHkv/cmVpbWFnaW5lLndl/YnA",
-        }}
-        style={{ height: 300, width: 300,resizeMode:'contain' }}
-      
-      />
-    </ScrollView>
-  );
-}
-export default HomeScreen;
+export default App;
---
+++ basic/notes/Basic.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import {
+  Text,
+  View,
+  Button,
+  Switch,
+  ScrollView,
+  TextInput,
+  Pressable,
+  Image,
+  ImageBackground,
+} from "react-native";
+
+function HomeScreen() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [text, setText] = useState("");
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  const pressMe = () => {
+    alert("Button Pressed");
+  };
+  const [timesPressed, setTimesPressed] = useState(0);
+
+  let textLog = "";
+  if (timesPressed > 1) {
+    textLog = timesPressed + "x onPress";
+  } else if (timesPressed > 0) {
+    textLog = "onPress";
+  }
+  return (
+    <ScrollView>
+      {/* {
+        [...Array(30)].map((_,i)=>(
+          <Text key={i} style={{fontSize:24}}>
+            Hello From Home Screen {i+1}
+          </Text>
+        ))
+      } */}
+
+      {/* <Text style={{ fontSize: 20, fontWeight: 'bold',marginBottom:20 }}>Home Screen</Text> */}
+
+      {/* <Button title='Click me' onPress={pressMe} /> */}
+
+      {/* <Switch
+          trackColor={{false: '#767577', true: '#81b0ff'}}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        /> */}
+
+      {/* <TextInput
+        style={{ backgroundColor: "aqua", borderWidth: 2 }}
+        value={text}
+        onChangeText={setText}
+        keyboardType="default"
+        multiline
+        numberOfLines={4}
+        placeholder="Enter Your Text"
+      /> */}
+
+      {/* <Pressable
+        onPress={() => {
+          setTimesPressed((current) => current + 1);
+        }}
+       
+      >
+        {({ pressed }) => (
+          <Text >{pressed ? "Pressed!" : "Press Me"}</Text>
+        )}
+      </Pressable>
+      <View >
+        <Text testID="pressable_press_console">{textLog}</Text>
+      </View> */}
+
+      {/* <Image
+        source={{
+          uri: "https://imgs.search.brave.com/ACedRZHztn-OEwyhM1B15tdkWFNDmr_vu6lbM9Pyr10/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9jZG4t/ZnJvbnQuZnJlZXBp/ay5jb20vaG9tZS9h/bm9uLXJ2bXAvY3Jl/YXRpdmUtc3VpdGUv/cGhvdG9ncmFwaHkv/cmVpbWFnaW5lLndl/YnA",
+        }}
+        style={{ height: 300, width: 300, resizeMode: "contain" }}
+      /> */}
+
+    </ScrollView>
+  );
+}
+export default HomeScreen;
