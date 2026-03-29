29ac5e31b45da0e3faeb6a8413f9b911a25290cf
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-29 12:59:03 +0530

    TextInput in react native

--- basic/app/index.tsx
+++ basic/app/index.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
-import {Text, View,Button, Switch, ScrollView } from 'react-native';
+import { useState } from "react";
+import {
+  Text,
+  View,
+  Button,
+  Switch,
+  ScrollView,
+  TextInput,
+} from "react-native";
 
-function HomeScreen(){
-  const [isEnabled, setIsEnabled] = useState(false)
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState)
+function HomeScreen() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [text, setText] = useState("");
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
-  const pressMe =()=>{
-    alert('Button Pressed')
-  }
-  return(
+  const pressMe = () => {
+    alert("Button Pressed");
+  };
+  return (
     <ScrollView>
       {/* {
         [...Array(30)].map((_,i)=>(
@@ -17,17 +25,26 @@ function HomeScreen(){
           </Text>
         ))
       } */}
+
       {/* <Text style={{ fontSize: 20, fontWeight: 'bold',marginBottom:20 }}>Home Screen</Text> */}
-      <Button title='Click me' onPress={pressMe} />
-      <Switch
+
+      {/* <Button title='Click me' onPress={pressMe} /> */}
+
+      {/* <Switch
           trackColor={{false: '#767577', true: '#81b0ff'}}
           thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
           ios_backgroundColor="#3e3e3e"
           onValueChange={toggleSwitch}
           value={isEnabled}
-        />
+        /> */}
+
+      <TextInput
+        style={{ backgroundColor: "aqua", height: 100, borderWidth: 2 }}
+        value={text}
+        onChangeText={setText}
+        keyboardType="phone-pad"
+      />
     </ScrollView>
-  )
+  );
 }
 export default HomeScreen;
-
