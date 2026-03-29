883c5d605c60f8d7a1b65d2921c6502b7decd4cb
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-29 12:47:47 +0530

    scroll view in react native

--- basic/app/index.tsx
+++ basic/app/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { useState } from 'react';
+import {Text, View,Button, Switch, ScrollView } from 'react-native';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+function HomeScreen(){
+  const [isEnabled, setIsEnabled] = useState(false)
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState)
+
+  const pressMe =()=>{
+    alert('Button Pressed')
+  }
+  return(
+    <ScrollView>
+      {/* {
+        [...Array(30)].map((_,i)=>(
+          <Text key={i} style={{fontSize:24}}>
+            Hello From Home Screen {i+1}
+          </Text>
+        ))
+      } */}
+      {/* <Text style={{ fontSize: 20, fontWeight: 'bold',marginBottom:20 }}>Home Screen</Text> */}
+      <Button title='Click me' onPress={pressMe} />
+      <Switch
+          trackColor={{false: '#767577', true: '#81b0ff'}}
+          thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+    </ScrollView>
+  )
 }
+export default HomeScreen;
+
