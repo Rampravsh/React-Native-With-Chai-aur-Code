450cf2e9d8858a5eb25c038eb14caf8ce38a3471
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-29 14:07:22 +0530

    FlatLIst in react native

--- basic/app/index.tsx
+++ basic/app/index.tsx
@@ -1,15 +1,62 @@
 import React from 'react';
-import {ImageBackground, StyleSheet, Text} from 'react-native';
+import {View, FlatList, StyleSheet, Text, StatusBar} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
-const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id:"58694a0f-3da1-471f-bd96-145571e29d72",
+    title: 'Third Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'fourth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'fifth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'sixth Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'seventh Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'eighth Item',
+  
+  }
+
+
+
+];
+
+type ItemProps = {title: string};
+
+const Item = ({title}: ItemProps) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const App = () => (
   <SafeAreaProvider>
-    <SafeAreaView style={styles.container} edges={['left', 'right']}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <Text style={styles.text}>Inside</Text>
-      </ImageBackground>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+      />
     </SafeAreaView>
   </SafeAreaProvider>
 );
@@ -17,18 +64,16 @@ const App = () => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
+  item: {
+    backgroundColor: '#44a580',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  text: {
-    color: 'white',
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
+  title: {
+    fontSize: 32,
   },
 });
 
